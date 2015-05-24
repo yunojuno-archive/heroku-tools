99f0ccdaf0aff020316565011cf84324aa894558
Hugo Rodger-Brown <hugo@rodger-brown.com>
2015-05-24 09:29:52 +0100

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 """Package setup for heroku-tools CLI application."""
+import os
+
 from setuptools import setup
 
 dependencies = ['click', 'envoy', 'yaml', 'requests']
 
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
 setup(
     name='heroku-tools',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/yunojuno/heroku-tools',
     license='MIT',
     author='Hugo Rodger-Brown',
     author_email='hugo@yunojuno.com',
     description=(
-        "Opinionated command line application for managing deployment "
-        "and configuration of Heroku applications."
+        "Command line application for managing Heroku applications."
     ),
+    long_description=README,
     include_package_data=True,
     packages=[
         'heroku_tools',
